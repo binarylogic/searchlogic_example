ba5860afc1a6c9f335759f741d5905f58b9dc1fc
Ben Johnson <bjohnson@binarylogic.com>
2008-09-07 16:46:35 -0400

    Changed default example to AJAX example

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.default '', :controller => "users"
+  map.default '', :controller => "orders"
   map.resources :users
   map.resources :orders
 end
